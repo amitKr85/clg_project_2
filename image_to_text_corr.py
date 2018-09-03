15aa5bd09955689ba31ef7537ffec0c3dc32bc3d
amitKr85 <42936000+amitKr85@users.noreply.github.com>
2018-09-03 17:41:07 +0530

    Add files via upload

---
+++ image_to_text_corr.py
@@ -0,0 +1,38 @@
+# for image proccessing
+from PIL import Image
+# for extract text
+from pytesseract import image_to_string
+# for check spelling
+from spellchecker import SpellChecker
+import re
+# for extract nouns
+from textblob import TextBlob
+
+
+print("extracting text from image....")
+# extracted text from image 
+text = image_to_string(Image.open('gfg_proj_ss.png'))
+
+print(text)
+
+print("correcting words....")
+# convert string into list of words
+#wordList = re.sub("[^\\w]", " ",  text).split()
+wordList = text.split()
+
+# correcting misspelled word
+spell = SpellChecker()
+#misspelled = spell.unknown(wordList)
+correctedText=''
+for word in wordList:
+	correctedText += spell.correction(word)+' '
+	#print(spell.candidates(word))
+
+print(correctedText)
+
+print("extracting nouns....")
+# extracting noun
+blob = TextBlob(correctedText)
+nounList = blob.noun_phrases
+
+print(nounList)
