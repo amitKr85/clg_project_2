ae476d3a5cf5db7b36131c9bd47041f30c212f8b
amit_kr <imamit2016kumar@gmail.com>
2019-01-01 23:28:51 +0530

    Revert "scripts backup"
    This reverts commit 152295962925fcf75995c388438ef04424b93364.

--- all scripts/image_to_text.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-from pytesseract import image_to_string
-#pytesseract.pytesseract.tesseract_cmd = 'F:\Tesseract-OCR\\tesseract.exe'
-
-#pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract'
-
-print(image_to_string(Image.open('developers_ss.png')))
